d5f61856ad97f9c32901f1aa01ff79d2a36fc442
BuildTools <unconfigured@null.spigotmc.org>
2025-04-21 19:49:56 +0300

    change gradle

--- Marrow/src/main/java/com/skeletonarmy/marrow/Hello.java
+++ Marrow/src/main/java/com/skeletonarmy/marrow/Hello.java
@@ -1,10 +1,7 @@
 package com.skeletonarmy.marrow;
 
 public class Hello {
-  
-  public static String GREETING = "Hello world!";
-  
   public static void main(String[] args) {
-    
-  }  
+
+  }
 }
