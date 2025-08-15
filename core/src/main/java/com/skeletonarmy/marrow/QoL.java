49db25b29507b0fd6b38fac803b885de9d6c7214
BuildTools <unconfigured@null.spigotmc.org>
2025-08-15 16:25:17 +0300

    feat: add QoL

---
+++ core/src/main/java/com/skeletonarmy/marrow/QoL.java
@@ -0,0 +1,51 @@
+package com.skeletonarmy.marrow;
+
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import java.util.List;
+
+public class QoL {
+    private static List<LynxModule> allHubs;
+
+    /**
+     * Sets the bulk caching mode for all Lynx modules in the hardware map.
+     * <p>
+     * <b>From GM0:</b> When in {@link LynxModule.BulkCachingMode#MANUAL} mode, if the cache is not cleared appropriately,
+     * stale values will be returned. For that reason, if you are not quite sure what you are doing, we recommend
+     * {@link LynxModule.BulkCachingMode#AUTO} mode; while {@link LynxModule.BulkCachingMode#MANUAL} mode can have some
+     * performance improvements if {@link LynxModule.BulkCachingMode#AUTO} mode is not used optimally, it has less room
+     * for catastrophic error.
+     *
+     * @param hardwareMap The hardware map.
+     * @param mode The desired bulk caching mode to be set.
+     */
+    public static void setBulkCachingMode(HardwareMap hardwareMap, LynxModule.BulkCachingMode mode) {
+        // Cache all hubs
+        allHubs = hardwareMap.getAll(LynxModule.class);
+
+        for (LynxModule hub : allHubs) {
+            hub.setBulkCachingMode(mode);
+        }
+    }
+
+    /**
+     * Clears the bulk data cache for all Lynx modules in the hardware map.
+     * <p>
+     * This is typically used when operating in {@link LynxModule.BulkCachingMode#MANUAL} mode to ensure fresh sensor and
+     * hardware values are retrieved. If the cache is not cleared in {@link LynxModule.BulkCachingMode#MANUAL} mode, stale
+     * data may be returned.
+     *
+     * @param hardwareMap The hardware map.
+     */
+    public static void clearBulkCache(HardwareMap hardwareMap) {
+        // Cache all hubs if not cached already
+        if (allHubs == null) {
+            allHubs = hardwareMap.getAll(LynxModule.class);
+        }
+
+        for (LynxModule hub : allHubs) {
+            hub.clearBulkCache();
+        }
+    }
+}
