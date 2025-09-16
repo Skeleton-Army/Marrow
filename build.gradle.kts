53f97481afe5f1fc76b087e9902fe26b125733ee
BuildTools <unconfigured@null.spigotmc.org>
2025-09-16 14:51:33 +0300

    fix: add support for publishing snapshots and to maven local

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,32 +14,55 @@ subprojects {
         projectInfo {
             name.set("Marrow")
             description.set("A lightweight library for building advanced robot behaviors.")
-            url.set("https://github.com/Skeleton-Army/Marrow")
-            scm {
-                fromGithub("Skeleton-Army", "Marrow")
-            }
+            url.set("https://skeleton-army.gitbook.io/marrow")
+            scm.fromGithub("Skeleton-Army", "Marrow")
             developer("Skeleton Army", "skeleton.army23644@gmail.com")
             license(MIT)
         }
 
-        centralPortalSpec {
-            auth.user.set(secret("CENTRAL_PORTAL_USER"))
-            auth.password.set(secret("CENTRAL_PORTAL_PASSWORD"))
-
-            signing.key.set(secret("SIGNING_KEY"))
-            signing.password.set(secret("SIGNING_PASSWORD"))
+        signing {
+            key.set(secret("SIGNING_KEY"))
+            password.set(secret("SIGNING_PASSWORD"))
         }
 
+        nexusSpec("snapshot") {
+            repositoryUrl.set("https://central.sonatype.com/repository/maven-snapshots/")
+
+            auth {
+                user.set(secret("CENTRAL_PORTAL_USER"))
+                password.set(secret("CENTRAL_PORTAL_PASSWORD"))
+            }
+        }
+
+        centralPortalSpec {
+            auth {
+                user.set(secret("CENTRAL_PORTAL_USER"))
+                password.set(secret("CENTRAL_PORTAL_PASSWORD"))
+            }
+        }
+
+        localSpec() // For publishing to Maven Local
+
         content {
-            androidComponents("release", "merged")
+            androidComponents("release")
         }
     }
 }
 
-tasks.register("publishToMavenCentral") {
+tasks.register("deployCentralPortal") {
+    group = "publishing"
+    description = "Publishes all subprojects to Maven Central."
     dependsOn(subprojects.map { it.tasks.named("deployCentralPortal") })
 }
 
-tasks.register("publishToMavenLocal") {
-    dependsOn(subprojects.map { it.tasks.named("publishToMavenLocal") })
+tasks.register("deployNexusSnapshot") {
+    group = "publishing"
+    description = "Publishes all subprojects to Maven Central Snapshots."
+    dependsOn(subprojects.map { it.tasks.named("deployNexusSnapshot") })
+}
+
+tasks.register("deployLocal") {
+    group = "publishing"
+    description = "Publishes all subprojects to Maven Local."
+    dependsOn(subprojects.map { it.tasks.named("deployLocal") })
 }
