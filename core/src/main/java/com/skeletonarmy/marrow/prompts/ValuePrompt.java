a56e709b5a78f2d0214b69841116eecdf78c5271
opi3636 <yoyozohar2010@gmail.com>
2025-10-03 16:26:56 +0300

    feat: Add a second increment to `ValuePrompt`

--- core/src/main/java/com/skeletonarmy/marrow/prompts/ValuePrompt.java
+++ core/src/main/java/com/skeletonarmy/marrow/prompts/ValuePrompt.java
@@ -8,6 +8,9 @@ public class ValuePrompt extends Prompt<Number> {
     private final double minValue;
     private final double maxValue;
     private final double increment;
+
+    // If bigIncrement isn't used it should be set to the same value as increment.
+    private final double bigIncrement;
     private Double selectedValue;
 
     // Flag that tells us whether this ValuePrompt should be treated as an integer prompt.
@@ -16,32 +19,38 @@ public class ValuePrompt extends Prompt<Number> {
     private final boolean isInteger;
 
     public ValuePrompt(String header) {
-        this(header, 0, Double.POSITIVE_INFINITY, 0, 1);
+        this(header, 0, Double.POSITIVE_INFINITY, 0, 1, 1);
     }
 
     public ValuePrompt(String header, double defaultValue) {
-        this(header, 0, Double.POSITIVE_INFINITY, defaultValue, 1);
+        this(header, 0, Double.POSITIVE_INFINITY, defaultValue, 1, 1);
     }
 
     public ValuePrompt(String header, double defaultValue, double increment) {
-        this(header, 0, Double.POSITIVE_INFINITY, defaultValue, increment);
+        this(header, 0, Double.POSITIVE_INFINITY, defaultValue, increment, increment);
     }
 
     public ValuePrompt(String header, double minValue, double maxValue, double defaultValue, double increment) {
+        this(header, minValue, maxValue, defaultValue, increment, increment);
+    }
+    public ValuePrompt(String header, double minValue, double maxValue, double defaultValue, double increment, double bigIncrement) {
         if (minValue >= maxValue) throw new IllegalArgumentException("Max value must be greater than min value.");
         if (defaultValue < minValue || defaultValue > maxValue) throw new IllegalArgumentException("Default value must be between min and max value.");
         if (increment <= 0) throw new IllegalArgumentException("Increment must be greater than zero.");
+        if (bigIncrement <= 0) throw new IllegalArgumentException("Big Increment must be greater then zero.");
 
         this.header = header;
         this.minValue = minValue;
         this.maxValue = maxValue;
         this.increment = increment;
+        this.bigIncrement = bigIncrement;
         this.selectedValue = defaultValue;
 
         this.isInteger = isIntegerLike(minValue) &&
                          isIntegerLike(maxValue) &&
                          isIntegerLike(defaultValue) &&
-                         isIntegerLike(increment);
+                         isIntegerLike(increment) &&
+                         isIntegerLike(bigIncrement);
     }
 
     @Override
@@ -55,10 +64,14 @@ public class ValuePrompt extends Prompt<Number> {
             addLine("< " + round(selectedValue, 2) + " >");
         }
 
-        if (pressAndHold(Button.DPAD_UP, 500, 100) || pressAndHold(Button.DPAD_RIGHT, 500, 100)) {
+        if (pressAndHold(Button.DPAD_UP, 500, 100)) {
             selectedValue = Math.min(maxValue, selectedValue + increment);
-        } else if (pressAndHold(Button.DPAD_DOWN, 500, 100) || pressAndHold(Button.DPAD_LEFT, 500, 100)) {
+        } else if (pressAndHold(Button.DPAD_RIGHT, 500, 100)) {
+            selectedValue = Math.min(maxValue, selectedValue + bigIncrement);
+        } else if (pressAndHold(Button.DPAD_DOWN, 500, 100)) {
             selectedValue = Math.max(minValue, selectedValue - increment);
+        } else if (pressAndHold(Button.DPAD_LEFT, 500, 100)) {
+            selectedValue = Math.min(minValue, selectedValue - bigIncrement);
         }
 
         if (justPressed(Button.A)) {
