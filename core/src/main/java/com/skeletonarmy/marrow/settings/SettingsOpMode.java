6298ab30819d07bc86a46ea85d0253b824c8259f
Nowammm <noamshemesh7@gmail.com>
2025-11-13 14:11:44 +0200

    feat: type conversion in get()

--- core/src/main/java/com/skeletonarmy/marrow/settings/SettingsOpMode.java
+++ core/src/main/java/com/skeletonarmy/marrow/settings/SettingsOpMode.java
@@ -1,5 +1,6 @@
 package com.skeletonarmy.marrow.settings;
 
+import com.google.gson.Gson;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.skeletonarmy.marrow.internal.Button;
 import com.skeletonarmy.marrow.internal.FileHandler;
@@ -13,10 +14,12 @@ import java.util.List;
 import java.util.Map;
 
 public abstract class SettingsOpMode extends OpMode {
-    private static final String FILE_DIR = "FIRST";
-    private static final String FILE_NAME = "marrow_settings.json";
+    private static final Gson GSON = new Gson();
+    private static final Map<String, Object> RESULTS = new HashMap<>();
 
-    private static final Map<String, Object> results = new HashMap<>();
+    private static String fileName;
+    private static String fileDir;
+
     private static boolean loaded = false;
 
     private final List<Setting<?>> settingPrompts = new ArrayList<>();
@@ -27,10 +30,21 @@ public abstract class SettingsOpMode extends OpMode {
 
     public abstract void defineSettings();
 
+    public String getFileName() {
+        return getClass().getSimpleName() + ".json";
+    }
+
+    public String getFileDir() {
+        return "FIRST/Settings";
+    }
+
     @Override
     public void init() {
+        fileName = getFileName();
+        fileDir = getFileDir();
+
         if (!loaded) {
-            FileHandler.loadFromFile(results, FILE_DIR, FILE_NAME);
+            FileHandler.loadFromFile(RESULTS, fileDir, fileName);
             loaded = true;
         }
 
@@ -41,11 +55,11 @@ public abstract class SettingsOpMode extends OpMode {
 
         for (Setting<?> setting : settingPrompts) {
             String key = setting.getKey();
-            cleanResults.put(key, results.get(key));
+            cleanResults.put(key, RESULTS.get(key));
         }
 
-        results.clear();
-        results.putAll(cleanResults);
+        RESULTS.clear();
+        RESULTS.putAll(cleanResults);
 
         telemetry.addLine("Press START to enter the menu.");
         telemetry.update();
@@ -67,13 +81,13 @@ public abstract class SettingsOpMode extends OpMode {
 
     @Override
     public void stop() {
-        FileHandler.saveToFile(results, FILE_DIR, FILE_NAME);
+        FileHandler.saveToFile(RESULTS, fileDir, fileName);
     }
 
     public <T> void add(String key, String name, Prompt<T> prompt) {
         for (Setting<?> existingSetting : settingPrompts) {
             if (existingSetting.getKey().equals(key)) {
-                throw new IllegalArgumentException("Duplicate key.");
+                throw new IllegalArgumentException("Duplicate key: " + key);
             }
         }
 
@@ -81,25 +95,54 @@ public abstract class SettingsOpMode extends OpMode {
     }
 
     @SuppressWarnings("unchecked")
-    public static <T> T get(String key) {
+    public static <T> T get(String key, Class<T> type, T defaultValue) {
         if (!loaded) {
-            FileHandler.loadFromFile(results, FILE_DIR, FILE_NAME);
+            FileHandler.loadFromFile(RESULTS, fileDir, fileName);
             loaded = true;
         }
 
-        Object value = results.get(key);
+        Object value = RESULTS.get(key);
+        if (value == null) return defaultValue;
 
-        // TODO: Handle nulls and defaults
+        // Enum conversion
+        if (type.isEnum() && value instanceof String) {
+            return (T) Enum.valueOf((Class<Enum>) type, (String) value);
+        }
+
+        // Primitive conversion (numbers)
+        if (value instanceof Number) {
+            Number number = (Number) value;
+            if (type == Integer.class) return (T) Integer.valueOf(number.intValue());
+            if (type == Long.class) return (T) Long.valueOf(number.longValue());
+            if (type == Float.class) return (T) Float.valueOf(number.floatValue());
+            if (type == Double.class) return (T) Double.valueOf(number.doubleValue());
+        }
+
+        // If value is JSON string and type is custom class
+        if (!(type.isInstance(value)) && value instanceof String) {
+            try {
+                return GSON.fromJson((String) value, type);
+            } catch (Exception e) {
+                throw new IllegalArgumentException("Failed to parse JSON for key '" + key + "'", e);
+            }
+        }
+
+        // Normal cast (String, etc.)
+        if (type.isInstance(value)) return (T) value;
+
         // TODO: Handle multiple settings opmodes having the same static results variable
 
-        return (T) value;
+        throw new IllegalArgumentException(
+                "Cannot cast setting '" + key + "' of type " + value.getClass().getSimpleName() +
+                        " to " + type.getSimpleName()
+        );
     }
 
     private void handleMenu() {
         int i = 0;
         for (Setting<?> setting : settingPrompts) {
             String settingName = setting.getName();
-            Object rawResult = get(setting.getKey());
+            Object rawResult = get(setting.getKey(), Object.class, null);
             String result = (rawResult != null) ? rawResult.toString() : "N/A";
             String cursor = (i == cursorIndex) ? " <" : "";
 
@@ -123,7 +166,17 @@ public abstract class SettingsOpMode extends OpMode {
             prompter = new Prompter(this);
             prompter.prompt("_", selected.getPrompt())
                     .onComplete(() -> {
-                        results.put(selected.getKey(), prompter.get("_"));
+                        Object value = prompter.get("_");
+
+                        // If enum, store as string
+                        if (value != null && value.getClass().isEnum()) value = value.toString();
+
+                            // If class, store as JSON string
+                        else if (value != null && !(value instanceof Number) && !(value instanceof String)) {
+                            value = GSON.toJson(value);
+                        }
+
+                        RESULTS.put(selected.getKey(), value);
                         currentState = State.MENU;
                     });
         }
