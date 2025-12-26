ab84ad5d86731f38c4a544ab8fc04234d2365296
Nowam <43613100+Nowammm@users.noreply.github.com>
2025-12-26 18:40:09 +0200

    chore: bump version to v1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ subprojects {
     apply(plugin = "io.deepmedia.tools.deployer")
 
     group = "com.skeletonarmyftc.marrow"
-    version = "1.0.0-SNAPSHOT"
+    version = "1.0.0"
 
     repositories {
         mavenCentral()
