3ce6e45039609363a1a57f5b184f8d0e09a3f3f6
opi3636 <yoyozohar2010@gmail.com>
2026-09-12 10:51:30 +0300

    feat: add decimal format to FormatBuilder

--- core/src/main/java/com/skeletonarmy/marrow/telemetry/FormatBuilder.java
+++ core/src/main/java/com/skeletonarmy/marrow/telemetry/FormatBuilder.java
@@ -8,6 +8,7 @@ import com.skeletonarmy.marrow.telemetry.modifiers.MultiConditionalModifier;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BooleanSupplier;
@@ -27,8 +28,17 @@ public class FormatBuilder {
         return modifiers;
     }
 
-    public FormatBuilder(String base) {
-        this.base = base;
+    public FormatBuilder(Object base) {
+        this(base, "0.####");
+    }
+
+    public FormatBuilder(Object base, String decimalFormat) {
+        if (base instanceof Double || base instanceof Float) {
+            this.base = new DecimalFormat(decimalFormat).format(base);
+        } else {
+            this.base = String.valueOf(base);
+        }
+
     }
 
     public FormatBuilder setColor(String hex) {
