14afe00ae38d169f272d2edcd52581421bd9b9c1
opi3636 <yoyozohar2010@gmail.com>
2026-09-24 20:05:58 +0300

    docs: fix annotation for BulkCachingSettings

--- core/src/main/java/com/skeletonarmy/marrow/BulkCachingSettings.java
+++ core/src/main/java/com/skeletonarmy/marrow/BulkCachingSettings.java
@@ -9,9 +9,6 @@ public class BulkCachingSettings {
     /**
      * The default bulk caching mode for all OpModes.
      * <p>
-     * Set this to {@link LynxModule.BulkCachingMode#OFF} to disable bulk caching globally
-     * by default (opt-in only via {@link BulkCaching} annotation).
-     * <p>
      * Defaults to {@link LynxModule.BulkCachingMode#OFF}.
      */
     public static LynxModule.BulkCachingMode defaultMode = LynxModule.BulkCachingMode.OFF;
